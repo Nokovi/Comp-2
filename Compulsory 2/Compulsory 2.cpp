fa4c1cd23f236b1234f36adc43e72797dac9ff10
Nokovi <sebny@online.no>
2023-09-19 08:03:02 +0200

    Wrote the first program

--- Compulsory 2/Compulsory 2.cpp
+++ Compulsory 2/Compulsory 2.cpp
@@ -2,20 +2,64 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-	std::cout << "hello world";
+long int factorial(long int number) {
+	
+	cout << "before" << number << "\n";
+	if (number == 0) {
+		return 1;
+		//factorial of 0 is 1... don't ask me why. Someone smarter than me could explain it. (too lazy to google it at 4 AM)
+	}
+	else {
+		number = number * factorial(number - 1);
+		//because a factorial n! = n*(n-1)*(n-2)*(n-3).... we can have it be recursive and just subtract 1 from the number we're multiplying the number with.
+		//it will do so until it reaches 0, then it returns 1 and it starts multiplying instead of creating more recursiveness.
+		cout << "after" << number << "\n";
+	}
+
+	return number;
+}
+
+int polynomial() {
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int calculator() {
+	return 0;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+int main()
+{
+	cout << "Calculator program.\nPlease input number to select an option.\n 1: Factorial \n 2: Polynomial \n 3: Simple math equations \n 4: Exit\n";
+	int select;
+	cin >> select;
+	if (!cin) { cout << "Woops!"; return 0; }
+	switch (select) {
+		case 1:
+			long int input;
+			cout << "Please input an integer below 13:\n";
+			cin >> input;
+			if (input > 12 || factorial(input) == 0) { 
+				cout << "Number too big!";
+				//12 is the cutoff point for where the answer would cause an overflow. I tried using long integers, but for some reason it keeps overflowing
+				//like a 32-bit integer. if you know why, please comment.
+				return 0;
+			}
+			cout << factorial(input);
+			break;
+		case 2:
+			long inputa;
+			cout << "Please input an integer:\n";
+			cin >> input;
+			cout << factorial(input);
+			polynomial();
+			break;
+		case 3:
+			calculator();
+			break;
+		case 4:
+			return 0;
+	}
+}
